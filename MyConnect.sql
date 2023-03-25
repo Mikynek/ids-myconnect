56c646f04415761a24626cbca7c9365bc9e0e5d0
Mikynek <mikysek.jakub@gmail.com>
2023-03-25 19:21:43 +0100

    feat: example data insert

--- MyConnect.sql
+++ MyConnect.sql
@@ -3,7 +3,7 @@
 --
 --
 DROP TABLE Uzivatel CASCADE CONSTRAINTS;
-DROP TABLE Pritelstvi CASCADE CONSTRAINTS;
+DROP TABLE Pratelstvi CASCADE CONSTRAINTS;
 DROP TABLE Konverzace CASCADE CONSTRAINTS;
 DROP TABLE Konverzace_ucastnici CASCADE CONSTRAINTS;
 DROP TABLE Zprava CASCADE CONSTRAINTS;
@@ -20,17 +20,17 @@ CREATE TABLE Uzivatel (
     mail VARCHAR(30) NOT NULL UNIQUE,
     jmeno VARCHAR(20) NOT NULL,
     prijmeni VARCHAR(20) NOT NULL,
-    datum_narozeni DATE NOT NULL,
-    pohlavi CHAR(1) CHECK (pohlavi IN ('M', 'Z')),
+    narozeni DATE NOT NULL,
+    pohlavi CHAR(7) CHECK (pohlavi IN ('M', 'Z')),
     mesto VARCHAR(20),
     ulice VARCHAR(20),
     cislo_popisne INTEGER,
     zamestnani VARCHAR(30),
     skola VARCHAR(30),
-    vztah CHAR(1) CHECK (vztah IN ('S', 'Z'))
+    vztah CHAR(5) CHECK (vztah IN ('S', 'Z'))
 );
 
-CREATE TABLE Pritelstvi ( -- unary  
+CREATE TABLE Pratelstvi ( -- unary  
     zacatek TIMESTAMP,
     uzivatel_id INTEGER NOT NULL,
     pritel_id INTEGER NOT NULL,
@@ -58,7 +58,7 @@ CREATE TABLE Konverzace_ucastnici (
 CREATE TABLE Zprava (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     obsah VARCHAR(255) NOT NULL,
-    datum TIMESTAMP NOT NULL, -- time included
+    datum TIMESTAMP NOT NULL,
     id_uzivatel INTEGER NOT NULL,
     FOREIGN KEY (id_uzivatel) REFERENCES Uzivatel(id), -- Change user, when deleting user to <DELETED USER>
     id_konverzace INTEGER NOT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE Akce (
     nazev VARCHAR(50) NOT NULL,
     datum DATE NOT NULL,
     misto VARCHAR(20),
-    typ_udalosti VARCHAR(1) CHECK(typ_udalosti IN ('F', 'V')), -- | F -> physical | V -> Virual |
+    typ_udalosti VARCHAR(12) CHECK(typ_udalosti IN ('F', 'V')), -- | F -> physical | V -> Virual |
     id_uzivatel INTEGER NOT NULL,
     FOREIGN KEY (id_uzivatel) REFERENCES Uzivatel(id) ON DELETE CASCADE
 );
@@ -87,7 +87,7 @@ CREATE TABLE Akce_ucastnici (
 CREATE TABLE Alba (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nazev VARCHAR(40) NOT NULL,
-    nastaveni_soukromi CHAR(1) CHECK (nastaveni_soukromi IN ('Y', 'N')),
+    nastaveni_soukromi CHAR(16) CHECK (nastaveni_soukromi IN ('public', 'private')),
     popis VARCHAR(255),
     id_uzivatel INTEGER NOT NULL,
     FOREIGN KEY (id_uzivatel) REFERENCES Uzivatel(id) ON DELETE CASCADE
@@ -98,7 +98,7 @@ CREATE TABLE Prispevek (
     datum DATE,
     misto VARCHAR(20),
     nadpis VARCHAR(40) NOT NULL,
-    popis VARCHAR(3071),
+    popis VARCHAR(511),
     id_uzivatel INTEGER NOT NULL,
     FOREIGN KEY (id_uzivatel) REFERENCES Uzivatel(id) ON DELETE CASCADE
 );
@@ -113,7 +113,7 @@ CREATE TABLE Prispevek_zminil (
 
 CREATE TABLE Fotky (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    panorama CHAR(1) CHECK (panorama IN ('Y', 'N')),
+    panorama CHAR(8) CHECK (panorama IN ('Y', 'N')),
     pomer_stran DECIMAL(4,2),
     id_prispevek INTEGER,
     FOREIGN KEY (id_prispevek) REFERENCES Prispevek(id) ON DELETE CASCADE, --INHERITANCE
@@ -137,3 +137,138 @@ ALTER TABLE Alba ADD (
     id_fotky INTEGER,
     CONSTRAINT id_fotky FOREIGN KEY (id_fotky) REFERENCES Fotky(id)
 );
+
+-- Fill Tables with Example Data
+
+INSERT INTO Uzivatel (mail, jmeno, prijmeni, narozeni, pohlavi, mesto, ulice, cislo_popisne, zamestnani, skola, vztah)
+VALUES ('johndoe@example.com', 'John', 'Doe', TO_DATE('1990-01-01','YYYY-MM-DD'), 'M', 'New York', 'Fifth Avenue', 10, 'Developer', 'MIT', 'S');
+
+INSERT INTO Uzivatel (mail, jmeno, prijmeni, narozeni, pohlavi, mesto, ulice, cislo_popisne, zamestnani, skola, vztah)
+VALUES ('janedoe@example.com', 'Jane', 'Doe', TO_DATE('1995-05-05','YYYY-MM-DD'), 'Z', 'Los Angeles', 'Hollywood Boulevard', 20, 'Designer', 'UCLA', 'S');
+
+INSERT INTO Uzivatel (mail, jmeno, prijmeni, narozeni, pohlavi, mesto, ulice, cislo_popisne, zamestnani, skola, vztah)
+VALUES ('johncena@example.com', 'John', 'Cena', TO_DATE('1977-04-23','YYYY-MM-DD'), 'M', 'West Newbury', 'Main Street', 15, 'Wrestler', NULL, 'Z');
+
+-- Pratelstvi
+
+INSERT INTO Pratelstvi (zacatek, uzivatel_id, pritel_id)
+VALUES (TO_DATE('2022-01-01 10:00:00', 'YYYY/MM/DD HH:MI:SS'), 1, 2);
+
+INSERT INTO Pratelstvi (zacatek, uzivatel_id, pritel_id)
+VALUES (TO_DATE('2022-01-01 11:00:00', 'YYYY/MM/DD HH:MI:SS'), 2, 3);
+
+INSERT INTO Pratelstvi (zacatek, uzivatel_id, pritel_id)
+VALUES (TO_DATE('2022-01-01 12:00:00', 'YYYY/MM/DD HH:MI:SS'), 3, 1);
+
+-- Konverzace
+
+INSERT INTO Konverzace (nazev, id_uzivatel)
+VALUES ('Hello world', 1);
+
+INSERT INTO Konverzace (nazev, id_uzivatel)
+VALUES ('Goodbye world', 2);
+
+INSERT INTO Konverzace (nazev, id_uzivatel)
+VALUES ('Testing 123', 3);
+
+-- Konverzace_ucastnici
+
+INSERT INTO Konverzace_ucastnici (zacatek, uzivatel_id, konverzace_id)
+VALUES (TO_DATE('2022-01-01 10:00:00', 'YYYY/MM/DD HH:MI:SS'), 1, 1);
+
+INSERT INTO Konverzace_ucastnici (zacatek, uzivatel_id, konverzace_id)
+VALUES (TO_DATE('2022-01-01 11:00:00', 'YYYY/MM/DD HH:MI:SS'), 2, 1);
+
+INSERT INTO Konverzace_ucastnici (zacatek, uzivatel_id, konverzace_id)
+VALUES (TO_DATE('2022-01-01 12:00:00', 'YYYY/MM/DD HH:MI:SS'), 3, 1);
+
+-- Tabulka Zprava
+INSERT INTO Zprava (obsah, datum, id_uzivatel, id_konverzace)
+VALUES ('Ahoj, jak se mas?', TO_DATE('2022-01-01 10:00:00', 'YYYY-MM-DD HH24:MI:SS'), 1, 1);
+INSERT INTO Zprava (obsah, datum, id_uzivatel, id_konverzace)
+VALUES ('Nic noveho, a ty?', TO_DATE('2022-01-01 10:05:00', 'YYYY-MM-DD HH24:MI:SS'), 2, 1);
+INSERT INTO Zprava (obsah, datum, id_uzivatel, id_konverzace)
+VALUES ('Taky nic zajimaveho', TO_DATE('2022-01-01 10:10:00', 'YYYY-MM-DD HH24:MI:SS'), 1, 1);
+INSERT INTO Zprava (obsah, datum, id_uzivatel, id_konverzace)
+VALUES ('Tento uzivatel neexistuje', TO_DATE('2022-01-01 10:15:00', 'YYYY-MM-DD HH24:MI:SS'), 3, 1);
+
+-- Tabulka Akce
+INSERT INTO Akce (nazev, datum, misto, typ_udalosti, id_uzivatel)
+VALUES ('Party u jezera', TO_DATE('2022-05-01', 'YYYY-MM-DD'), 'Jezero', 'F', 1);
+INSERT INTO Akce (nazev, datum, misto, typ_udalosti, id_uzivatel)
+VALUES ('Ples', TO_DATE('2022-02-14', 'YYYY-MM-DD'), 'Palac Zofin', 'F', 2);
+INSERT INTO Akce (nazev, datum, misto, typ_udalosti, id_uzivatel)
+VALUES ('Stream koncert', TO_DATE('2022-04-10', 'YYYY-MM-DD'), 'Online', 'V', 1);
+INSERT INTO Akce (nazev, datum, misto, typ_udalosti, id_uzivatel)
+VALUES ('Workshop', TO_DATE('2022-03-05', 'YYYY-MM-DD'), 'Domov', 'V', 3);
+
+-- Tabulka Akce_ucastnici
+INSERT INTO Akce_ucastnici (zacatek, uzivatel_id, akce_id)
+VALUES (TO_DATE('2022-05-01 20:00:00', 'YYYY-MM-DD HH24:MI:SS'), 1, 1);
+INSERT INTO Akce_ucastnici (zacatek, uzivatel_id, akce_id)
+VALUES (TO_DATE('2022-05-01 20:00:00', 'YYYY-MM-DD HH24:MI:SS'), 2, 1);
+INSERT INTO Akce_ucastnici (zacatek, uzivatel_id, akce_id)
+VALUES (TO_DATE('2022-02-14 19:00:00', 'YYYY-MM-DD HH24:MI:SS'), 2, 2);
+INSERT INTO Akce_ucastnici (zacatek, uzivatel_id, akce_id)
+VALUES (TO_DATE('2022-04-10 21:00:00', 'YYYY-MM-DD HH24:MI:SS'), 1, 3);
+
+-- Tabulka Alba
+INSERT INTO Alba (nazev, nastaveni_soukromi, popis, id_uzivatel)
+VALUES ('Moje dovolená 2022', 'private', 'Fotky z dovolené na Bali', 1);
+INSERT INTO Alba (nazev, nastaveni_soukromi, popis, id_uzivatel)
+VALUES ('Rodinné oslavy', 'private', 'Fotky z rodinných oslav', 2);
+INSERT INTO Alba (nazev, nastaveni_soukromi, popis, id_uzivatel)
+VALUES ('Mé oblíbené restaurace', 'public', 'Fotografie z mých oblíbených restaurací', 3);
+
+-- Tabulka Prispevek
+INSERT INTO Prispevek (datum, misto, nadpis, popis, id_uzivatel)
+VALUES (TO_DATE('2022-01-15', 'YYYY-MM-DD'), 'Praha', 'Můj výlet do Prahy', 'V Praze jsem si prohlédl staré město a navštívil několik muzeí.', 1);
+INSERT INTO Prispevek (datum, misto, nadpis, popis, id_uzivatel)
+VALUES (TO_DATE('2022-03-20', 'YYYY-MM-DD'), 'Brno', 'Návštěva v Brně', 'V Brně jsem navštívil krásnou zoologickou zahradu a ochutnal místní speciality.', 2);
+INSERT INTO Prispevek (datum, misto, nadpis, popis, id_uzivatel)
+VALUES (TO_DATE('2022-05-05', 'YYYY-MM-DD'), 'Plzeň', 'Pivní festival v Plzni', 'Na pivním festivalu v Plzni jsem ochutnal mnoho druhů piva a poznal zajímavé lidi.', 3);
+
+-- Tabulka Prispevek_zminil
+INSERT INTO Prispevek_zminil (uzivatel_id, prispevek_id)
+VALUES (1, 1);
+INSERT INTO Prispevek_zminil (uzivatel_id, prispevek_id)
+VALUES (2, 1);
+INSERT INTO Prispevek_zminil (uzivatel_id, prispevek_id)
+VALUES (1, 2);
+INSERT INTO Prispevek_zminil (uzivatel_id, prispevek_id)
+VALUES (2, 2);
+INSERT INTO Prispevek_zminil (uzivatel_id, prispevek_id)
+VALUES (3, 2);
+
+-- Tabulka Fotky
+INSERT INTO Fotky (panorama, pomer_stran, id_prispevek, id_alba)
+VALUES ('N', 1.5, NULL, 1);
+INSERT INTO Fotky (panorama, pomer_stran, id_prispevek, id_alba)
+VALUES ('N', 1.33, NULL, 2);
+INSERT INTO Fotky (panorama, pomer_stran, id_prispevek, id_alba)
+VALUES ('Y', 2.5, NULL, 2);
+
+-- Tabulka Videa
+INSERT INTO Videa (kvalita, delka, FPS, id_prispevek, id_alba)
+VALUES (1080, TO_DATE('01:15:00', 'HH24:MI:SS'), 60, NULL, 1);
+INSERT INTO Videa (kvalita, delka, FPS, id_prispevek, id_alba)
+VALUES (720, TO_DATE('00:45:00', 'HH24:MI:SS'), 30, NULL, 2);
+INSERT INTO Videa (kvalita, delka, FPS, id_prispevek, id_alba)
+VALUES (480, TO_DATE('00:30:00', 'HH24:MI:SS'), 30, 1, NULL);
+
+UPDATE Alba SET id_fotky = 1 WHERE id = 1;
+UPDATE Alba SET id_fotky = 3 WHERE id = 2;
+
+-- Select Test
+SELECT * FROM Uzivatel;
+SELECT * FROM Pratelstvi;
+SELECT * FROM Konverzace;
+SELECT * FROM Konverzace_ucastnici;
+SELECT * FROM Zprava;
+SELECT * FROM Akce;
+SELECT * FROM Akce_ucastnici;
+SELECT * FROM Alba;
+SELECT * FROM Prispevek;
+SELECT * FROM Prispevek_zminil;
+SELECT * FROM Fotky;
+SELECT * FROM Videa;
